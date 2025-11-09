30cd96a3395cf2edc8319e38b99e2da2f1eebff4
Johita86 <yohanitaullua@gmail.com>
2025-11-09 03:49:39 -0300

    Corregir carpeta para deploy: eliminar carpeta antigua y agregar portfolio-jvu

--- portfoliojvu/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2025 Johana Vanina Ullua | Desarrolladora Full Stack 💜</p>
-    </footer>
-  )
-}
-
-export default Footer
