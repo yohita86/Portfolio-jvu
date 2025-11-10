51cbdce331ffa450412db6ebe1ea5a24946ed80c
Johita86 <yohanitaullua@gmail.com>
2025-11-09 22:04:17 -0300

    Cambio padding en header para mejor vista de titulos

--- portfolio-jvu/vite.config.js
+++ portfolio-jvu/vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <- Esto es clave para que las rutas relativas funcionen
+  base: '', // 👈 sin "./" ni "/"
 })
